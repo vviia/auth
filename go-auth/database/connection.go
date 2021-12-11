520495fe2948c594ea9bcc2406c315c8d0e457ac
aulcode <viamadeline@gmail.com>
2021-12-11 15:14:24 +0700

    first commit

---
+++ go-auth/database/connection.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"go-auth/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("root:rootroot@/go-auth"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
